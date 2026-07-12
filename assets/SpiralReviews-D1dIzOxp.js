4311528b1fca661a0a6660e1d955048498a944de
Harisrizvi <haris28.hr@gmail.com>
2026-07-13 04:58:01 +0500

    Deploy

--- assets/SpiralReviews-D1dIzOxp.js
+++ assets/SpiralReviews-D1dIzOxp.js
@@ -1,4 +1,4 @@
-import{a as Mn,j as rt,A as ul}from"./motion-vendor-BlUQMVQx.js";import{m as fl,r as mi}from"./index-DzXnJRNQ.js";import"./react-vendor-CVQSmgK8.js";/**
+import{a as Mn,j as rt,A as ul}from"./motion-vendor-BlUQMVQx.js";import{m as fl,r as mi}from"./index-C6dQdPgv.js";import"./react-vendor-CVQSmgK8.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
